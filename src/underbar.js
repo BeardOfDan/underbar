3c8d9438c604c82001bfedec10ec5b6f908e344b
Daniel Roberts <BeardOfDan@gmail.com>
2017-09-09 11:33:36 -0700

    Fix 'each' test

--- src/underbar.js
+++ src/underbar.js
@@ -53,6 +53,18 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function (collection, iterator) {
+    if (Object.prototype.toString.call(collection) === "[object Array]") {
+      for (let i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else { // is an object
+      const keys = Object.keys(collection);
+      for (let i = 0; i < keys.length; i++) {
+        const key = keys[i];
+        iterator(collection[key], key, collection);
+      }
+
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
