1e82f26c35b6267b808a30ad589c95f2f4b26a7b
Daniel Roberts <BeardOfDan@gmail.com>
2017-09-13 12:45:59 -0700

    Fix 'every' test
    Note: This is the start of Part II

--- src/underbar.js
+++ src/underbar.js
@@ -207,6 +207,14 @@
   // Determine whether all of the elements match a truth test.
   _.every = function (collection, iterator) {
     // TIP: Try re-using reduce() here.
+
+    // provide a default value for iterator
+    iterator = iterator ? iterator : _.identity;
+
+    return _.reduce(collection, function (passed, item) {
+      if (!passed) { return false; }
+      return Boolean(iterator(item));
+    }, true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
